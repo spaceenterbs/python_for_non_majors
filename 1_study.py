d22d63e80c7fb7fb5bd52cd9306ddc58db84142e
enterspacebs <anotherhell48@gmail.com>
2023-10-02 01:45:06 +0900

    객체명 정하기 들어가자

---
+++ 1_study.py
@@ -0,0 +1,21 @@
+print("hello, world!")
+
+print("-" + "3")
+
+10  # 추리하기 01
+print(10)  # 추리하기 02
+# print[10] # 추리하기 03
+# print{10} # 추리하기 04
+# print 10 # 추리하기 05
+# PRINT(10) # 추리하기 06
+
+print(10)
+print(20)
+print(30)
+
+print(10, 20, 30)
+print(10, 20, 30, sep=":")  # sep=":" : 구분자를 콜론(:)으로 지정, sep=" " : 구분자를 공백으로 지정(기본값)
+
+# 정리하기 04
+print(10, end="_")
+print(20, end="_")
---
+++ 2_variable.py
@@ -0,0 +1,22 @@
+# 변수의 의미와 역할 이해하기
+
+# 추리하기 01
+print(300)
+print(300, 300)
+print(300, 500)
+print(500, 300)
+print(300, 500, 300)
+
+# 추리하기 02
+a = 300
+b = 500
+print(a)
+print(a, a)
+print(a, b)
+print(b, a)
+print(a, b, a)
+
+# 응용하기 01
+apple = 30
+banana = 20
+print(apple, banana, 10)
